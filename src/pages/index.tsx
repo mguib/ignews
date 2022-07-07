1abb5f209b2cd991bf6423d133278f9da7cb32c8
Marcos Guibson <43528733+mguib@users.noreply.github.com>
2022-07-07 16:24:19 -0400

    feat: Craindo projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title> Inicio | ig.News</title>
+
+      </Head>
+      <h1>
+
+        Hello <span>Word</span>
+      </h1>
+    </>
+  )
+}
